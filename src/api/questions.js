7ccccf1432ce8a61f9f7b361c31c66b537cf9647
oBLAZERo2001 <hari9112001@gmail.com>
2023-12-03 16:53:38 +0530

    get questions api intro

--- src/api/questions.js
+++ src/api/questions.js
@@ -1,11 +1,28 @@
 import { default as axios } from "axios";
 
-export const createQuestion = async function (data) {
-	const SERVER_URL = process.env.REACT_APP_SERVER_URL;
+const SERVER_URL = process.env.REACT_APP_SERVER_URL;
 
+export const createQuestion = async function (data) {
 	try {
 		let token = localStorage.getItem("token");
 		const response = await axios.post(SERVER_URL + "questions", data, {
+			headers: {
+				"Content-Type": `application/json`,
+				Authorization: "Bearer " + token,
+			},
+		});
+		if (response.status === 200) {
+			return response.data;
+		}
+	} catch (error) {
+		console.log(error.message);
+	}
+};
+
+export const getQuestions = async function () {
+	try {
+		let token = localStorage.getItem("token");
+		const response = await axios.get(SERVER_URL + "questions", {
 			headers: {
 				"Content-Type": `application/json`,
 				Authorization: "Bearer " + token,
--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -1,74 +1,85 @@
+import { getQuestions } from "../api/questions";
 import "../styles/Home.css";
 import "../styles/navbar.css";
-import { Avatar, Box, Menu, MenuItem } from "@mui/material";
-import React, { useEffect, useState } from "react";
+import { Avatar, Box } from "@mui/material";
+import { useEffect, useState } from "react";
 
 export const Home = () => {
-	const data = [0, 1, 2];
+	const [data, setData] = useState([]);
+
+	const getQuestionsData = async () => {
+		const res = await getQuestions();
+		if (res?.length > 0) setData(res);
+	};
+
+	useEffect(() => {
+		getQuestionsData();
+	}, []);
 
 	return (
 		<Box>
-			{data.map((d) => {
-				return (
-					<Box
-						sx={{
-							color: "black",
-							mb: 2,
-							borderBottom: "0.5px dashed #c9c9c9",
-							pb: 2,
-						}}
-					>
-						<Box mb={1} display={"flex"} alignItems={"center"}>
-							<Avatar sx={{ width: 32, height: 32 }} />
-							<Box
-								style={{
-									fontSize: "12px",
-									fontWeight: "500",
-									color: "black",
-								}}
-								ml={1}
-							>
-								<p style={{ color: "black" }}>@leostelon</p>
-								<small>4:53PM 12th Nov 2023</small>
-							</Box>
-						</Box>
+			{data?.length > 0 &&
+				data.map((d) => {
+					return (
 						<Box
 							sx={{
-								display: "flex",
-								justifyContent: "center",
-								alignItems: "center",
+								color: "black",
+								mb: 2,
+								borderBottom: "0.5px dashed #c9c9c9",
+								pb: 2,
 							}}
 						>
-							<Box>
-								<p style={{ marginTop: "4px" }}>Some title of the article.</p>
-								<p
+							<Box mb={1} display={"flex"} alignItems={"center"}>
+								<Avatar sx={{ width: 32, height: 32 }} />
+								<Box
 									style={{
-										color: "grey",
-										marginTop: "6px",
-										textAlign: "justify",
+										fontSize: "12px",
+										fontWeight: "500",
+										color: "black",
 									}}
+									ml={1}
 								>
-									Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde
-									ratione nemo sit explicabo? Harum quibusdam provident
-									voluptas. Quod, cumque. Laborum veritatis laboriosam natus
-									eum. Perspiciatis itaque odio quod dolorem distinctio?
-								</p>
+									<p style={{ color: "black" }}>@leostelon</p>
+									<small>4:53PM 12th Nov 2023</small>
+								</Box>
 							</Box>
 							<Box
 								sx={{
-									minWidth: "100px",
-									height: "100px",
-									borderRadius: "8px",
-									ml: 2,
-									backgroundImage: `url(${"https://miro.medium.com/v2/resize:fit:1100/format:webp/1*Q9GGI2LPnPN-DF15FBeNiA.jpeg"})`,
-									backgroundPosition: "center",
-									backgroundSize: "cover",
+									display: "flex",
+									justifyContent: "center",
+									alignItems: "center",
 								}}
-							></Box>
+							>
+								<Box>
+									<p style={{ marginTop: "4px" }}>Some title of the article.</p>
+									<p
+										style={{
+											color: "grey",
+											marginTop: "6px",
+											textAlign: "justify",
+										}}
+									>
+										Lorem ipsum dolor sit amet consectetur adipisicing elit.
+										Unde ratione nemo sit explicabo? Harum quibusdam provident
+										voluptas. Quod, cumque. Laborum veritatis laboriosam natus
+										eum. Perspiciatis itaque odio quod dolorem distinctio?
+									</p>
+								</Box>
+								<Box
+									sx={{
+										minWidth: "100px",
+										height: "100px",
+										borderRadius: "8px",
+										ml: 2,
+										backgroundImage: `url(${"https://miro.medium.com/v2/resize:fit:1100/format:webp/1*Q9GGI2LPnPN-DF15FBeNiA.jpeg"})`,
+										backgroundPosition: "center",
+										backgroundSize: "cover",
+									}}
+								></Box>
+							</Box>
 						</Box>
-					</Box>
-				);
-			})}
+					);
+				})}
 		</Box>
 	);
 };
